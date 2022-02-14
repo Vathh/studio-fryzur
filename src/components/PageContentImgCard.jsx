b017fa1734062790eb320bf70f3873f911c9b6dd
Maciej <eloth@vp.pl>
2022-02-14 07:01:00 +0100

    added onclick handler for img cards

--- src/components/PageContentImgCard.jsx
+++ src/components/PageContentImgCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PageContentImg = ({beforeImg, afterImg}) => {
+
+  const [isReversed, setIsReversed] = useState(false);
+
+  const reverseHandler = () => {
+    setIsReversed(!isReversed);
+  }
+
+  const styles = {
+    backgroundImage: `url(${afterImg})`,
+    clipPath: isReversed ? 'circle(120% at 50% 90%)' : 'circle(0 at 50% 90%)'
+  }
+
   return ( 
     <div className="page__card" style={{backgroundImage: `url(${beforeImg})`}}>
-      <button className="page__card-btn"><i className="fas fa-eye"></i></button>
-      <div className="page__card-reverse" style={{backgroundImage: `url(${afterImg})`}}>         
+      <button className="page__card-btn" onClick={reverseHandler}><i className="fas fa-eye"></i></button>
+      <div className="page__card-reverse" style={styles}>         
       </div>
     </div>
    );
 }
  
 export default PageContentImg;
+
+// {backgroundImage: `url(${afterImg})`}, isReversed ? {clipPath: 'circle(120% at 50% 90%)'} : {clipPath: 'none'}
